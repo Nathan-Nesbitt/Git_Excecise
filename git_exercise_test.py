b81034ad0450ff01824a1af4e8daae74c7034395
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2020-09-11 14:37:00 -0700

    Added second test for integer_sort

--- git_exercise_test.py
+++ git_exercise_test.py
@@ -6,12 +6,20 @@ from git_exercise import Sort
 class TestSort(unittest.TestCase):
 
     def test_integer_sort(self):
+        """
+            Tests to see if default functionality works.
+        """
         sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         sort = Sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
 
-        print(sort.interger_sort())
-
         self.assertEqual(sorted_array, sort.interger_sort())
+
+    def test_integer_sort_type(self):
+        """
+            Tests to see if wrong data-types are caught.
+        """
+        sort = Sort(["a", "b", "c"])
+        self.assertRaises(TypeError, sort.interger_sort)
 
 if __name__ == "__main__":
     unittest.main()
